aec63d9fb8a1b5ee3264bcbefe6e9caddf0f060c
satish <satishkonduru0225@gmail.com>
2025-06-06 11:46:35 +0530

    ngModel and Signal

---
+++ src/app/components/temp-ref-variables/temp-ref-variables.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+@Component({
+  selector: 'app-temp-ref-variables',
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  templateUrl: './temp-ref-variables.component.html',
+  styleUrl: './temp-ref-variables.component.css',
+})
+export class TempRefVariablesComponent {
+  message = '';
+  getInputValue(value: any) {
+    this.message = value;
+  }
+  onInput(e: Event) {
+    const val = (e.target as HTMLInputElement).value;
+    this.message = val;
+  }
+}
