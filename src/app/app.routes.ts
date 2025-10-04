633ffe30f8cbdea74eab2a7106a623604035b0bc
satish <satishkonduru0225@gmail.com>
2025-10-04 10:24:35 +0530

    Route Parameters

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { CourseComponent } from './components/course/course.component';
 import { CourseDetailsComponent } from './components/course-details/course-details.component';
+import { DocsComponent } from './components/docs/docs.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   {
@@ -15,5 +17,13 @@ export const routes: Routes = [
   {
     path: 'courseDetails',
     component: CourseDetailsComponent,
+  },
+  {
+    path: 'docs',
+    component: DocsComponent,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
   },
 ];
